b44029aec8ac23236e2f8ede476320cb62acab12
unknown <urrutia_987@hotmail.com>
2023-09-28 15:17:33 -0600

    datecafe-backend

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            'nombre' => 'Bebidas',
+            'icono' => 'bebidas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Pupusas',
+            'icono' => 'pupusas',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        DB::table('categorias')->insert([
+            'nombre' => 'Postres',
+            'icono' => 'postre',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+    }
+}
