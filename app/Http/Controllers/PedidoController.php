dbc79314bf01ce1c1d03ddb8d1e3f89016d7d3a6
unknown <urrutia_987@hotmail.com>
2023-09-29 16:07:50 -0600

    v1.02

---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\Pedido;
+use App\Models\PedidoProducto;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //Guardar pedido
+        $pedido = new Pedido;
+        $pedido->user_id = Auth::user()->id;
+        $pedido->total = $request->total;
+        $pedido->save();
+
+        //Obtener ID
+        $id = $pedido->id;
+        //Obtener productos de la orden
+        $productos = $request->productos;
+
+        //Formatear el arreglo con los ids y las cantidades
+        $pedido_producto = [];
+        foreach($productos as $producto) {
+            $pedido_producto[] = [
+                'pedido_id' => $id,
+                'producto_id' => $producto['id'],
+                'cantidad' => $producto['cantidad'],
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ];
+        }
+
+        //guardar en la base de datos
+        PedidoProducto::insert($pedido_producto);
+
+        return [
+            "message" => 'Pedido Realizado, estara listo en unos minutos'
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pedido $pedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pedido $pedido)
+    {
+        //
+    }
+}
