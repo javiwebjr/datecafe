6edbc697af927c517dabbc33e61e252e53be5da2
unknown <urrutia_987@hotmail.com>
2023-10-02 09:13:52 -0600

    antes de delivery

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PedidoCollection;
 use Carbon\Carbon;
 use App\Models\Pedido;
 use App\Models\PedidoProducto;
@@ -15,7 +16,7 @@ class PedidoController extends Controller
      */
     public function index()
     {
-        //
+        return new PedidoCollection(Pedido::with('user')->with('productos')->where('estado', 0)->get());
     }
 
     /**
@@ -67,7 +68,11 @@ class PedidoController extends Controller
      */
     public function update(Request $request, Pedido $pedido)
     {
-        //
+        $pedido->estado = 1;
+        $pedido->save();
+        return [
+            'pedido' => $pedido
+        ];
     }
 
     /**
